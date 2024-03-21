0ecdb5b92f6cc2af53d772b0d0d5fb7209154cda
Joy Ahmed <dev.joyahmed@gmail.com>
2024-03-22 01:32:07 +0600

    Delete unused route.tsx file and add bcrypt dependency

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,36 @@
+import prisma from "@/lib/prisma";
+import { AuthOptions } from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
+import * as bcrypt from "bcrypt";
+import NextAuth from "next-auth/next";
+
+
+export const authOptions: AuthOptions = {
+    providers:[
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "username" },
+                password: {  label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                const user = await prisma.user.findUnique({
+                    where:{
+                        email: credentials?.username
+                    }
+                })
+
+                if(!user) throw new Error("No user found");
+                // const isPasswordCorrect = credentials?.password === user.password;
+                if(!credentials?.password) throw new Error("Please enter your password");
+                const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
+                if(!isPasswordCorrect) throw new Error("Wrong password");
+                const { password, ...rest } = user;
+                return rest;
+            }
+        })
+    ]
+}
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
