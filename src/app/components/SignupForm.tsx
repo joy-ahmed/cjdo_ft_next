e7313c6b41ce98d7af9b92426044b76523a8907b
Joy Ahmed <dev.joyahmed@gmail.com>
2024-03-22 04:57:26 +0600

    Add @hookform/resolvers and zodResolver to SignupForm

--- src/app/components/SignupForm.tsx
+++ src/app/components/SignupForm.tsx
@@ -9,69 +9,102 @@ import {
   EyeSlashIcon,
   PaperAirplaneIcon,
 } from "@heroicons/react/24/solid";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Checkbox, Input } from "@nextui-org/react";
 import Link from "next/link";
 import React from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 
-const FormSchema = z.object({
-  firstName: z
-    .string()
-    .min(2, { message: "first name must be at least 2 characters" })
-    .max(20, { message: "first name must be less than 20 characters" })
-    .regex(new RegExp("[a-zA-Z]"), "first name must be only letters"),
-  lastName: z
-    .string()
-    .min(2, { message: "last name must be at least 2 characters" })
-    .max(20, { message: "last name must be less than 20 characters" })
-    .regex(new RegExp("[a-zA-Z]"), "last name must be only letters"),
-  email: z.string().email("Please enter a valid email"),
-  phone: z.string().regex(new RegExp("[0-9]"), "phone must be only numbers"),
-  password: z
-    .string()
-    .min(6, { message: "password must be at least 6 characters" })
-    .max(20, { message: "password must be less than 20 characters" }),
-  confirmPassword: z
-    .string()
-    .min(6, { message: "password must be at least 6 characters" })
-    .max(20, { message: "password must be less than 20 characters" }),
-  acceptTerms: z.literal(true, {
-    errorMap: () => ({ message: "You must accept the terms and conditions" }),
-  }),
-}).refine((data) => data.password === data.confirmPassword, {
-  message: "Passwords do not match",
-  path: ["confirmPassword"],
-})
+const FormSchema = z
+  .object({
+    firstName: z
+      .string()
+      .min(2, { message: "first name must be at least 2 characters" })
+      .max(20, { message: "first name must be less than 20 characters" })
+      .regex(new RegExp("[a-zA-Z]"), "first name must be only letters"),
+    lastName: z
+      .string()
+      .min(2, { message: "last name must be at least 2 characters" })
+      .max(20, { message: "last name must be less than 20 characters" })
+      .regex(new RegExp("[a-zA-Z]"), "last name must be only letters"),
+    email: z.string().email("Please enter a valid email"),
+    phone: z.string().regex(new RegExp("[0-9]"), "phone must be only numbers"),
+    password: z
+      .string()
+      .min(6, { message: "password must be at least 6 characters" })
+      .max(20, { message: "password must be less than 20 characters" }),
+    confirmPassword: z
+      .string()
+      .min(6, { message: "password must be at least 6 characters" })
+      .max(20, { message: "password must be less than 20 characters" }),
+    acceptTerms: z.literal(true, {
+      errorMap: () => ({ message: "You must accept the terms and conditions" }),
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+type InputType = z.infer<typeof FormSchema>;
 
 const SignupForm = () => {
+  const { register, handleSubmit, reset, control, formState: { errors } } = useForm<InputType>({
+    resolver: zodResolver(FormSchema),
+  });
   const [isvissiblePassword, setIsVisiblePassword] = React.useState(false);
+
+  const saveUser: SubmitHandler<InputType> = async (data) => {
+    console.log(data);
+    reset();
+  };
+
   const togglePassword = () => setIsVisiblePassword(!isvissiblePassword);
+
   return (
-    <form className="grid grid-cols-2 border border-gray-900 shadow-md place-self-stretch rounded-md gap-3 p-4">
+    <form
+      onSubmit={handleSubmit(saveUser)}
+      className="grid grid-cols-2 border border-gray-900 shadow-md place-self-stretch rounded-md gap-3 p-4"
+    >
       <Input
-        isRequired
+        errorMessage={errors?.firstName?.message}
+        isInvalid={!!errors.firstName}
+        {...register("firstName")}
         label="first name"
         startContent={<UserIcon className="w-4" />}
+        defaultValue=""
       />
       <Input
-        isRequired
+        errorMessage={errors?.lastName?.message}
+        isInvalid={!!errors.lastName}
+        {...register("lastName")}
         label="last name"
         startContent={<UserIcon className="w-4" />}
+        defaultValue=""
       />
       <Input
-        isRequired
+        errorMessage={errors?.email?.message}
+        isInvalid={!!errors.email}
+        {...register("email")}
         className="col-span-2"
         label="email"
         startContent={<EnvelopeIcon className="w-4" />}
+        defaultValue=""
       />
       <Input
-        isRequired
+        errorMessage={errors?.phone?.message}
+        isInvalid={!!errors.phone}
+        {...register("phone")}
         className="col-span-2"
         label="phone"
         startContent={<PhoneIcon className="w-4" />}
+        defaultValue=""
       />
       <Input
-        isRequired
+        errorMessage={errors?.password?.message}
+        isInvalid={!!errors.password}
+        {...register("password")}
         className="col-span-2"
         type={isvissiblePassword ? "text" : "password"}
         label="password"
@@ -85,22 +118,39 @@ const SignupForm = () => {
             )}
           </button>
         }
+        defaultValue=""
       />
       <Input
-        isRequired
+        errorMessage={errors?.confirmPassword?.message}
+        isInvalid={!!errors.confirmPassword}
+        {...register("confirmPassword")}
         className="col-span-2"
         type={isvissiblePassword ? "text" : "password"}
         label="confirm password"
         startContent={<KeyIcon className="w-4" />}
+        defaultValue=""
       />
-      <Checkbox className="col-span-2" isRequired>
-        I accept the{" "}
-        <Link className="underline" href="#">
-          terms and conditions
-        </Link>
-      </Checkbox>
+      <Controller
+        control={control}
+        name="acceptTerms"
+        render={({ field }) => (
+          <Checkbox
+            onChange={field.onChange}
+            onBlur={field.onBlur}
+            className="col-span-2"
+          >
+            I accept the{" "}
+            <Link className="underline" href="#">
+              terms and conditions
+            </Link>
+          </Checkbox>
+        )}
+      />
+      {errors.acceptTerms && (
+        <p className="col-span-2 text-red-500">{errors.acceptTerms.message}</p>
+      )}
       <div className="col-span-2 flex justify-self-end">
-        <Button color="primary" className="w-48">
+        <Button type="submit" color="primary" className="w-48">
           Sign Up
           <PaperAirplaneIcon className="w-4" />
         </Button>
